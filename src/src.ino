56a5dec3e28e42fa372e31f0eb7d61110fb1b40d
Julian <jmclower@edu.uwaterloo.ca>
2018-08-25 14:31:47 -0400

    Added more register definitions

--- src/src.ino
+++ src/src.ino
@@ -5,14 +5,19 @@ int SS = 10;
 
 // define register numbers from data sheet
 
-#define DEV_ID       0x00 // device address not needed for SPI
-#define THRESH_TAP   0x1D // threshold value for tap interupts
-#define OFFSET_X     0x1E // offset on x axis
-#define OFFSET_Y     0x1F // offset on y axis
-#define OFFSET_Z     0x20 // offset on z axis
-#define DUR          0x21 // time duration an event must be above THRESH_TAP to qualify as tap event
-#define LATENT       0x22 // time gap between tap detection and time window start
-#define WINDOW       0x23 // tap window
+#define DEV_ID         0x00 // device address not needed for SPI
+#define THRESH_TAP     0x1D // threshold value for tap interupts
+#define OFFSET_X       0x1E // offset on x axis
+#define OFFSET_Y       0x1F // offset on y axis
+#define OFFSET_Z       0x20 // offset on z axis
+#define DUR            0x21 // time duration an event must be above THRESH_TAP to qualify as tap event
+#define LATENT         0x22 // time gap between tap detection and time window start
+#define WINDOW         0x23 // tap window
+#define THRESH_ACTl    0x24 // threshold value for detecting activity
+#define THRESH_INACT   0x25 // threshold value for detecting inactivity
+#define TIME_INACT     0x26 // time that acceleration must be less than THRESH_INACT for inactivity to be declared
+#define ACT_INACT_CTL  0x27 // enable/disable axes from detecting activity or inactivity, ac/dc
+#define THRESH_FF      0x29 // 
 
 void setup()
 {
