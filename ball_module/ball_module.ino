232f86abea62ef0f9f80ae757ffb233f5af17355
Julian <jmclower@edu.uwaterloo.ca>
2019-09-10 22:59:14 -0400

    Added lcd to ball module, debug, ready for accel logic test

--- ball_module/ball_module.ino
+++ ball_module/ball_module.ino
@@ -2,6 +2,8 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <LiquidCrystal.h>
+
 #include "ball_module.h"
 
 // create RF24 object and initialize CE, CSN pins
@@ -14,39 +16,48 @@ const byte pipe[6] = "00001";
 int SS_nrf = 3;
 
 // array of bytes for reading from adxl
-char output[512];                                     // what is this for
+//char output[512];                                     // what is this for
 
 // x y z axis measurements
-float xRaw, yRaw, zRaw = 0;
+float xRaw, yRaw, zRaw = 0; 
 float x, y, z = 0;
 float sumAcc = 0;
 
 // array to hold acceleration values
 float acc_values[200];
+bool exceeded_array = false;
 
 // acc_counter for indexing through acc_values
 int acc_counter = 0;
 
 // time values for speed calculation
-long long t1, t2 = 0;
+unsigned long long t1, t2 = 0;
 
 // velocity used as final calculation
 int throw_speed = 0;
+bool failed = false;
 
+//LCD pins
+const int rs = A0, en = A1, d4 = A2, d5 = A3, d6 = 9, d7 = 10;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // ------------------------------------------------------------- MAIN --------------------------------------------------------------
 void setup()
 {
+  lcd.begin(16, 2); 
+  
   // set output mode for slave select pins
-  pinMode(SS_nrf, OUTPUT);
-  digitalWrite(SS_nrf, HIGH);
+//  pinMode(SS_nrf, OUTPUT);
+//  digitalWrite(SS_nrf, HIGH);
 
-  // initialize SPI bus and set mode (clock polarity 1, clock phase 1)
-//  SPI.begin();
-//  SPI.setDataMode(SPI_MODE0);
+/*
+ initialize SPI bus and set mode (clock polarity 1, clock phase 1)
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0);
+*/
 
   // initialize serial monitor
-  Serial.begin(9600);
+//  Serial.begin(9600);
 
   Wire.begin();
   // put adxl345 into measurement mode
@@ -66,13 +77,14 @@ void setup()
   Wire.endTransmission();
 
 
-//  // put adxl345 into measurement mode
-//  writeTo(POWER_CTL, 0b00001000, SS_acc);
-//  // set range to +/-4G
-//  writeTo(DATA_FORMAT, 0b00000001, SS_acc);
-//  // set output data rate to 100Hz
-//  writeTo(BW_RATE, 0b00001010, SS_acc);
-
+/*
+  // put adxl345 into measurement mode
+  writeTo(POWER_CTL, 0b00001000, SS_acc);
+  // set range to +/-4G
+  writeTo(DATA_FORMAT, 0b00000001, SS_acc);
+  // set output data rate to 100Hz
+  writeTo(BW_RATE, 0b00001010, SS_acc);
+*/
 
 
 
@@ -111,23 +123,20 @@ void loop()
   sumAcc -= 9.803;
 
   // enter statement if throw is detected
-  if(sumAcc > 20)
+  if(sumAcc > 15)
   {
     // get initial time
     t1 = millis();
     // set acc_counter to 0
     acc_counter = 0;
     // stay in loop while ball is being accelerated
-    while(sumAcc > 4)
+    while(sumAcc > 5)
     {
       // keep acceleration values in array and increment acc_counter
       acc_values[acc_counter] = sumAcc;
       acc_counter++;
 
-      Serial.println(acc_counter);
-
-
-
+//      Serial.println(acc_counter);
 
       // Recalculate sumAcc value
       read_six_reg(DATA_X_LSB, x, y, z);
@@ -146,63 +155,80 @@ void loop()
       sumAcc = sqrt((x * x) + (y * y) + (z * z));
       sumAcc -= 9.803;
 
-
-
-
-
+      if(acc_counter >= 200){
+        exceeded_array = true;
+        break;
+      }
 
       
     }
     // get final time
     t2 = millis();
     // add all acceleration values from array
-    int acc_vals_sum = 0;
-    for(int index = 0; index < acc_counter; index++)
-    {
+    unsigned int acc_vals_sum = 0;
+    for(int index = 0; index < acc_counter; index++){
       acc_vals_sum += acc_values[index];
     }
     // calculate average acceleration, time, and final velocity of throw
     int acc_avg = acc_vals_sum / acc_counter;
-    int throw_time = (t1-t2) * 1000;
+    double throw_time = (t1-t2) / 1000;
     throw_speed = acc_avg * throw_time;
 
     // send data to display module
-    radio.write(&throw_speed, sizeof(throw_speed));
+//    radio.write(&throw_speed, sizeof(throw_speed));
+
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("TS:");
+    lcd.setCursor(4,0);
+    lcd.print(throw_speed);
+    lcd.setCursor(7,0);
+    lcd.print("TT");
+    lcd.setCursor(10,0);
+    lcd.print(throw_time);
+    lcd.setCursor(0,1);
+    lcd.print("NUM VALS:");
+    lcd.setCursor(10,1);
+    lcd.print(acc_counter);
+
+    while(1)
+    {}
   }
+
+//  if(!radio.write(&sumAcc, sizeof(sumAcc)))
+//  {
+//    failed = true;
+//  }
+  delay(100);
+
+    double throw_time = 12;
+  
+//    lcd.clear();
+//    lcd.setCursor(0,0);
+//    lcd.print("TS:");
+//    lcd.setCursor(4,0);
+//    lcd.print(throw_speed);
+//    lcd.setCursor(7,0);
+//    lcd.print("TT");
+//    lcd.setCursor(10,0);
+//    lcd.print(throw_time);
+//    lcd.setCursor(0,1);
+//    lcd.print("NUM VALS:");
+//    lcd.setCursor(10,1);
+//    lcd.print(acc_counter);
+
+
  
-  Serial.print(x);
-  Serial.print(" ");
-  Serial.print(y);
-  Serial.print(" ");
-  Serial.print(z);
-  Serial.print("   ");
-  Serial.print(sumAcc);
-  Serial.print("   ");
-  Serial.println(throw_speed);
-  delay(1000);
+//  Serial.print(x);
+//  Serial.print(" ");
+//  Serial.print(y);
+//  Serial.print(" ");
+//  Serial.print(z);
+//  Serial.print("   ");
+//  Serial.println(sumAcc);
+
+  
+//  Serial.print("   ");
+//  Serial.println(throw_speed);
+//  delay(500);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
